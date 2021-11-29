4f0718fc15b3ad399c2c3a7aa2b1fb383c9e55f4
thomaskiely <54604314+thomaskiely@users.noreply.github.com>
2021-11-29 15:01:48 -0500

    finished bot and formatted results
    takes in user input from discord channel and sends that location to the weather api call, and fetches all weather data and returns it back to the discord channel in a formatted sentence.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord import message
 from discord.ext import commands
 from discord.message import Message
 import requests, json
+import time
 
 
 client = commands.Bot(command_prefix='!')
@@ -16,18 +17,33 @@ async def on_ready():
 @client.command()
 async def weather(ctx, *, content:str):
 
+    #read the location from the user
     location = content
     print(location)
 
     
+    #get weather data from getWeather function
+    weatherList = getWeather(location)
+
+    #assign elements of weatherList to new variables
+    #(description,temperature,feels like temp, humidity, wind speed, cloud percentage,sunset time)
+
+    description = weatherList[0]
+    temperature = kelvinToFarenHeit(weatherList[1])
+    feelsLike = kelvinToFarenHeit(weatherList[2])
+    humidity = weatherList[3]
+    #convert m/s to mph
+    windSpeed = round(weatherList[4]*2.237)
+    cloudPercentage = weatherList[5]
+    sunsetTime = weatherList[6]
+    sunsetTimeFormatted = epochToNormalTime(sunsetTime)
+
+
+
+    await ctx.send("Weather in "+location+ " is "+description + ". Temperature is "+str(temperature)+"°F, "+ " feels like "+str(feelsLike)+"°F. Wind speed is "+str(windSpeed)+"mph. Humidity is "+ str(humidity)+"%. Cloudiness is "+ str(cloudPercentage)+"%. Sunset at "+ str(sunsetTimeFormatted)+" PM EST.")
+
+
     
-    weath = getWeather(location)
-
-    farWeath = kelvinToFarenHeit(weath)
-
-    weathString = str(farWeath)
-
-    await ctx.send("Weather in "+location+ " is "+weathString+"°F")
 
 
 
@@ -45,7 +61,12 @@ def kelvinToFarenHeit(kelvinTemp):
     return farenheitTemp
 
 
+
+#convert epoch time to 24 hour
+def epochToNormalTime(epochTime):
     
+    formattedTime = time.strftime('%H:%M',time.localtime(epochTime))
+    return formattedTime
     
 
 
@@ -54,12 +75,16 @@ def kelvinToFarenHeit(kelvinTemp):
 
 
 def getWeather(locationString):
+    #API KEY for weather
     myKey = "KEY"
 
+    #base API call that we will fill in with criteria
     baseWeather = "https://api.openweathermap.org/data/2.5/weather?"
 
+    #assign the user input to city variable to use in the API call
     city = locationString
 
+    #create link for API call
     weatherCall = baseWeather + "q=" + city + "&appid=" + myKey
 
     #http request
@@ -68,24 +93,54 @@ def getWeather(locationString):
     if response.status_code == 200:
         data = response.json()
 
+        #get main weather data
         main = data['main']
 
+        #get temperature data
         temperature = main['temp']
 
+        #get feels like temp
+        feelsLike = main['feels_like']
+
+        #get humidity
+        humidity = main['humidity']
+
+        #get wind data
         wind = data['wind']
 
+        #get wind speed
         windSpeed = wind['speed']
 
-        print(f"Temperature: {temperature}")
-       
-        print(f"Wind Speed: {windSpeed}")
+        #get clouds data
+        cloudData = data['clouds']
+
+        #get cloudiness percentage
+        cloudPercentage = cloudData['all']
+
+
+        #get description
+        getDescription = data['weather']
+        description = getDescription[0]['description']
+        description2 = getDescription[0]['main']
+
+
+        #get sunset time
+        getTimes = data['sys']
+        sunset = getTimes['sunset']
+
 
     else:
         print("error in http request")
         print(response.status_code)
         print(weatherCall)
 
-    return temperature
+
+    #return a list of the following
+    #(description,temperature,feels like temp, humidity, wind speed, cloud percentage,sunset time)
+    weatherItems = [description2,temperature,feelsLike,humidity,windSpeed,cloudPercentage,sunset]
+
+    print(weatherItems)
+    return weatherItems
 
 
 
